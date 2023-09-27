9c49f37e073fe71f75782e1b1a5542271ce8a71d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-27 16:39:38 +0300

    Registered the connection string for the application

--- PlatformService.Platform/Program.cs
+++ PlatformService.Platform/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Platform.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<PlatformDbContext>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("PlatformDbConnString"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("PlatformDbConnString"))); ;
+});
 
 var app = builder.Build();
 
