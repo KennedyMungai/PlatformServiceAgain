970e43cd9ca37de06ac7aab2679342dfd3938e36
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-28 10:04:17 +0300

    Created the file to contain the Dto classes

---
+++ PlatformService.Platform/Models/Dtos/PlatformDtos.cs
@@ -0,0 +1,9 @@
+namespace PlatformService.Platform.Models.Dtos;
+
+public class PlatformReadDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Publisher { get; set; } = string.Empty;
+    public decimal Cost { get; set; }
+}
