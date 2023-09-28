f1e46da5b5a8f1c9011f7c8ea6caba4214517164
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-28 10:07:19 +0300

    Added the PlatformCreateDto class

--- PlatformService.Platform/Models/Dtos/PlatformDtos.cs
+++ PlatformService.Platform/Models/Dtos/PlatformDtos.cs
@@ -7,3 +7,10 @@ public class PlatformReadDto
     public string Publisher { get; set; } = string.Empty;
     public decimal Cost { get; set; }
 }
+
+public class PlatformCreateDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Publisher { get; set; } = string.Empty;
+    public decimal Cost { get; set; }
+}
