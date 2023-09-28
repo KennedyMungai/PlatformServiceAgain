7a10e62f131db5f268fe03f83601f0a423dac103
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-28 10:08:06 +0300

    Registered the Map for the PlatformCreateDto

--- PlatformService.Platform/Models/Dtos/Profiles/PlatformProfile.cs
+++ PlatformService.Platform/Models/Dtos/Profiles/PlatformProfile.cs
@@ -8,5 +8,6 @@ public class PlatformProfile : Profile
     public PlatformProfile()
     {
         CreateMap<PlatformModel, PlatformReadDto>().ReverseMap();
+        CreateMap<PlatformModel, PlatformCreateDto>().ReverseMap();
     }
 }
