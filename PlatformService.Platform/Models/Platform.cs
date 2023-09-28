707b229efec8bea0ae5a80349efa58d9d81db493
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-28 09:41:40 +0300

    Using Guids instead of ints for the primary key field

--- PlatformService.Platform/Models/Platform.cs
+++ PlatformService.Platform/Models/Platform.cs
@@ -6,7 +6,7 @@ public class PlatformModel
 {
     [Key]
     [Required]
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     [Required]
     public string Name { get; set; } = string.Empty;
     [Required]
