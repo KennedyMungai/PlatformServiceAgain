0a7363ca73abbf82c30b2d5a786e3d3eb1483c48
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-28 12:34:49 +0300

    The basic code for the commands controller

---
+++ PlatformService.Command/Controllers/CommandsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlatformService.Command.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CommandsController : ControllerBase
+{
+
+}
